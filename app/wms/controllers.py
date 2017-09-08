b827c88426e692d24a1a03a0fc7528bdb859d2b0
KryTerNexT <kryternext@gmail.com>
2017-09-09 03:45:20 +0400

    Added initial version of log in.

--- app/wms/controllers.py
+++ app/wms/controllers.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, redirect, flash, abort
+from flask_login import logout_user, login_user
 from settings import USER, PASSWORD, SMTP
+from model import User
+
 from abc import ABCMeta
 import smtplib
 from email.mime.text import MIMEText
@@ -23,15 +26,48 @@ class UserController(Controller):
         Controller for users
     """
 
-    def __init__(self, request):
+    def __init__(self, request, user=None):
+        self.user = user
         super(UserController, self).__init__(request)
+
+    def init_user(self):
+        if "username" in self.request.form and "password" in self.request.form:
+            username = self.request.form['username']
+            password = self.request.form['password']
+            if password == username + "_":
+                self.user = User(1)
+
+    def login(self):
+        self.init_user()
+        if self.request.method == "POST":
+            if self.user is not None:
+                login_user(self.user)
+                next_page = self.request.args.get("next")
+                if next_page is None:
+                    return redirect("/home")
+                else:
+                    return redirect(next_page)
+            else:
+                return abort(401)
+        else:
+            return render_template(
+                "login.html",
+            )
+
+    def logout(self):
+        logout_user()
+        flash("Logged out!")
+        return render_template(
+            "login.html",
+        )
 
     def index(self):
         return render_template(
             "pages/home.html",
             site={
                 "title": "Warehouse Management System"
-            }
+            },
+            user=self.user
         )
 
 
@@ -50,6 +86,9 @@ class StaticPageController(Controller):
                 "title": "Contact"
             }
         )
+
+    def register(self):
+        pass
 
 
 class ServiceController(Controller):
--- app/wms/run_app.py
+++ app/wms/run_app.py
@@ -1,8 +1,13 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, flash
+from flask_login import login_required, LoginManager
+from model import User
 from settings import DEBUG
 from controllers import UserController, StaticPageController, ServiceController
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 app.secret_key = 'ThisIsMyGraduationWork:"WarehouseManagementSystem"'
 app.template_folder = "files/templates"
 app.static_folder = "files/static"
@@ -16,9 +21,22 @@ def index():
 
 
 @app.route("/home", methods=["GET", "POST"])
+@login_required
 def home():
     controller = UserController(request)
     return controller.index()
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    controller = UserController(request)
+    return controller.login()
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    controller = StaticPageController(request)
+    return controller.register()
 
 
 @app.route("/contact", methods=["GET", "POST"])
@@ -31,6 +49,24 @@ def contact():
 def send_feedback():
     controller = ServiceController(request)
     return controller.send_feedback()
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    controller = UserController(request)
+    return controller.logout()
+
+
+@app.errorhandler(401)
+def page_not_found(e):
+    flash("Username or password incorrect")
+    return redirect("/login")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
 
 
 if __name__ == "__main__":
